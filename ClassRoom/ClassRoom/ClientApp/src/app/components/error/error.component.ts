6cbd8c1ce4f9fcd813ee7c4cd2e0642db3eece06
yannick.frischknecht <yannick.frischknecht@basenet.ch>
2020-06-24 11:44:06 +0200

    removed styling

--- ClassRoom/ClassRoom/ClientApp/src/app/components/error/error.component.ts
+++ ClassRoom/ClassRoom/ClientApp/src/app/components/error/error.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  templateUrl: './error.component.html'
 })
 export class ErrorComponent implements OnInit {
 
